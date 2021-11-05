06897b13f22012767e0f1d97886ec13040f600f5
iago isi <iago_isi@hotmail.com>
2021-11-05 16:22:55 -0400

    :zap: -->criacao da API de Produtos

--- models/index.js
+++ models/index.js
@@ -2,3 +2,4 @@ require("./loja");
 require("./user");
 require("./cliente");
 require("./categoria");
+require("./produto");
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -4,5 +4,6 @@ router.use("/users", require("./users"));
 router.use("/users", require("./clientes"));
 router.use("/lojas", require("./lojas"));
 router.use("/categorias", require("./categorias"));
+router.use("/produtos", require("./produtos"));
 
 module.exports = router;
